ca7c99dfae216d0f8e2749bfeffa5394174d886b
Hathamunage Harsha Sandaruwan <74146694+Harsha-hathamunage@users.noreply.github.com>
2023-04-05 22:59:06 +0100

    commiting final code & all files except final report
    commiting final code and all the other files except final report

--- Assignment_02_ADS1_Harsha_Sandaruwan_Hathamunage.py
+++ Assignment_02_ADS1_Harsha_Sandaruwan_Hathamunage.py
@@ -7,9 +7,11 @@ Created on Mon Mar 27 21:05:43 2023
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as stats
+import seaborn as sns
 
 
-def process_data(url):
+def dataframe_maker(url):
     """
     The function reads data from a CSV file at the provided URL and filters it
     based on desired indicators, countries, and years, resulting in two cleaned
@@ -28,23 +30,21 @@ def process_data(url):
     # Read data from CSV file
     df = pd.read_csv(url, skiprows=4)
 
-    # Define the list of South Asian countries
-    south_asia = ['Afghanistan', 'Bangladesh', 'Bhutan',
-                  'India', 'Maldives', 'Nepal', 'Pakistan', 'Sri Lanka']
+    # Define the list of BRICS countries
+    brics = ['Brazil', 'Russia', 'India', 'China', 'South Africa']
 
     # Define the list of desired indicators
-    desired_indicators = ['Total greenhouse gas emissions (kt of CO2 equivalent)',
-                          'Arable land (% of land area)',
+    desired_indicators = ['Arable land (% of land area)',
                           'Forest area (% of land area)',
-                          'Average precipitation in depth (mm per year)']
+                          'Population growth (annual %)']
 
     # Filter the original dataframe by countries list and desired indicators
-    df_south_asia = df[(df['Country Name'].isin(south_asia)) & (
+    df_brics = df[(df['Country Name'].isin(brics)) & (
         df['Indicator Name'].isin(desired_indicators))]
 
     # Select the desired years and set them as columns
     years = ['2000', '2005', '2010', '2015', '2018']
-    df_years = df_south_asia.loc[:, [
+    df_years = df_brics.loc[:, [
         'Country Name', 'Country Code', 'Indicator Name'] + years]
 
     # Remove rows with missing values
@@ -63,8 +63,53 @@ def process_data(url):
 url = 'https://github.com/Harsha-hathamunage/ADS1---Assignment-2-Statistics-and-trends/raw/main/Climate%20Change%20Dataset.csv'
 
 # Call the function with the URL argument
-df_years, df_countries = process_data(url)
+df_years, df_countries = dataframe_maker(url)
 
 # Print the two resulting dataframes
 print(df_years)
 print(df_countries)
+
+# Save two DataFrames as a CSV files
+df_years.to_csv('df_years.csv')
+df_countries.to_csv('df_countries.csv')
+
+# Compute summary statistics for each indicator and country
+stats = df_years.groupby(['Indicator Name', 'Country Name']).describe()
+
+# Print the summary statistics for each indicator and country
+print(stats)
+
+# Group the data by Indicator Name
+grouped_data = df_years.groupby(['Indicator Name'])
+
+# Create a bar chart for each indicator
+for name, group in grouped_data:
+    # Set up the plot
+    fig, ax = plt.subplots()
+    # add a title to the plot
+    plt.title(name)
+    ax.set_xticklabels(df_years['Country Name'].unique())
+    ax.set_xticks(np.arange(len(df_years['Country Name'].unique())))
+    ax.set_xlabel('Country Name')
+    ax.set_ylabel(name)
+
+    # Plot the data
+    for i, year in enumerate(['2000', '2005', '2010', '2015', '2018']):
+        ax.bar(np.arange(len(group['Country Name'].unique())) + i*0.15,
+               group[year].values,
+               width=0.15,
+               label=year)
+
+    # Add a legend
+    ax.legend()
+
+    # Show the plot
+    plt.show()
+
+df_pivot = df_years.pivot_table(values=['2015'],
+                                index=['Country Name', 'Country Code'],
+                                columns='Indicator Name')
+df_pivot.columns = df_pivot.columns.droplevel(0)
+corr_matrix = df_pivot.corr()
+
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
